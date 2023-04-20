3cd3780ddcbb0cab0b4cb114bd526274ce1f54de
Alua196-xyz <xyz@odoo.com>
2023-04-21 02:18:20 +0600

    Home Page

---
+++ src/routers/Routers.js
@@ -0,0 +1,31 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+
+
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+import Favourites from '../pages/Favourites'
+import ProductDetails from '../pages/ProductDetails'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+
+
+
+const Routers = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to='home' />} />
+
+            <Route path='home' element={<Home />} />
+            <Route path='products' element={<Products />} />
+            <Route path='products/:id' element={<ProductDetails />} />
+            <Route path='favourites' element={<Favourites />} />
+            <Route path='checkout' element={<Checkout />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+        </Routes>
+    );
+};
+
+export default Routers;
